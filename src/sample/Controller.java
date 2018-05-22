9adbeae1afe4bbd1389b423ea72a8768ff393b9f
PitKwang <PitKwang@gmail.com>
2018-05-22 16:46:00 +0700

    DB Connection update

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,11 +4,13 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 import javax.swing.*;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
@@ -21,28 +23,46 @@ public class Controller implements Initializable {
     private PasswordField password;
     @FXML
     private Button btnLogin;
+    @FXML
+    private Label dbStatus;
 
-
-
+    private loginModel loginModel = new loginModel();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        if (this.loginModel.isDatabaseConnection()){
+            this.dbStatus.setText("Connected to DB.");
+        }else{
+            this.dbStatus.setText("Not Connect DB.");
+        }
     }
 
     @FXML
     public void Login(ActionEvent event){
-        try {
+//        try {
+//            if (id.equals(username.getText()) && pass.equals(password.getText())){
+//                JOptionPane.showMessageDialog(null,"Welcome to Twitter");
+//            }
+//            else {
+//                JOptionPane.showMessageDialog(null,"Your Username or Password is invalid","Warning Message", JOptionPane.WARNING_MESSAGE);
+//            }
+//        } catch (Exception e){
+//            e.printStackTrace();
+//        }
+
+
+        try{
+            if (this.loginModel.isLogin(this.username.getText(),this.password.getText()));
             if (id.equals(username.getText()) && pass.equals(password.getText())){
                 JOptionPane.showMessageDialog(null,"Welcome to Twitter");
-            }
-            else {
+            }else {
                 JOptionPane.showMessageDialog(null,"Your Username or Password is invalid","Warning Message", JOptionPane.WARNING_MESSAGE);
             }
-        } catch (Exception e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
-    }//Login
 
+
+    }//Login
 
 }//class
